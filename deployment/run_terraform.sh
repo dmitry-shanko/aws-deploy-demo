4f41aa2c772479c85e899498030cd95146386451
dmitry.shanko <shankodmitry@gmail.com>
2026-01-10 23:18:56 +0400

    Removing VPC_id

--- deployment/run_terraform.sh
+++ deployment/run_terraform.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 set -e
 
-VPC_ID="$1"
-SUBNET_ID="$2"
-SG_ID="$3"
-IAM_PROFILE="$4"
-INSTANCE_TYPE="$5"
-ELASTIC_IP_ID="$6"
-KEY_NAME="$7"
+SUBNET_ID="$1"
+SG_ID="$2"
+IAM_PROFILE="$3"
+INSTANCE_TYPE="$4"
+ELASTIC_IP_ID="$5"
+KEY_NAME="$6"
 
 cd "$(dirname "$0")/terraform"
 
 echo "[INFO] Running Terraform with:"
-echo "  VPC: $VPC_ID"
 echo "  Subnet: $SUBNET_ID"
 echo "  SG: $SG_ID"
 echo "  IAM Profile: $IAM_PROFILE"
@@ -23,7 +21,6 @@ echo "  Key: $KEY_NAME"
 terraform init -input=false
 
 terraform apply -input=false -auto-approve \
-  -var="vpc_id=$VPC_ID" \
   -var="subnet_id=$SUBNET_ID" \
   -var="security_group_id=$SG_ID" \
   -var="instance_profile=$IAM_PROFILE" \
